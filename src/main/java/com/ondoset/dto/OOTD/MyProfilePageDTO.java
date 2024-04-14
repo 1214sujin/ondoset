37c58c09be3fe24def683b81c63cb5c8c1163c32
Sujin Kang <1214sujin@naver.com>
2024-04-15 04:51:59 +0900

    feature: 내 프로필 페이징 구현

---
+++ src/main/java/com/ondoset/dto/OOTD/MyProfilePageDTO.java
@@ -0,0 +1,24 @@
+package com.ondoset.dto.OOTD;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class MyProfilePageDTO {
+
+	@Getter
+	@Setter
+	public static class req {
+
+		private Long lastPage;
+	}
+
+	@Getter
+	@Setter
+	public static class res {
+
+		private Long lastPage;
+		private List<OotdDTO> ootdList;
+	}
+}
