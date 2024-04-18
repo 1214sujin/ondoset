38a5dfb693bbf2e7e3a54d1e1a7ca40ee0e9818e
Sujin Kang <1214sujin@naver.com>
2024-04-18 20:53:43 +0900

    fix: 패키지명 수정(소문자) 및 기타 수정 - JPQL 쿼리문

--- src/main/java/com/ondoset/repository/OOTDRepository.java
+++ src/main/java/com/ondoset/repository/OOTDRepository.java
@@ -15,11 +15,11 @@ public interface OOTDRepository extends JpaRepository<OOTD, Long> {
 
 	Long countByMember(Member member);
 
-	@Query("select new com.ondoset.dto.OOTD.OotdDTO(o.id, (o.departTime/86400+1)*86400-:timezone/1000 date, o.lowestTemp, o.highestTemp, o.imageURL) " +
+	@Query("select new com.ondoset.dto.ootd.OotdDTO(o.id, (o.departTime/86400+1)*86400-:timezone/1000 date, o.lowestTemp, o.highestTemp, o.imageURL) " +
 			"from OOTD o where o.member=:member and o.reportedCount<5 and o.isBlinded=false order by o.id desc limit 10")
 	List<OotdDTO> pageMyProfile(@Param("timezone") int timezone, @Param("member") Member member);
 
-	@Query("select new com.ondoset.dto.OOTD.OotdDTO(o.id, (o.departTime/86400+1)*86400-:timezone/1000 date, o.lowestTemp, o.highestTemp, o.imageURL) " +
+	@Query("select new com.ondoset.dto.ootd.OotdDTO(o.id, (o.departTime/86400+1)*86400-:timezone/1000 date, o.lowestTemp, o.highestTemp, o.imageURL) " +
 			"from OOTD o where o.member=:member and o.reportedCount<5 and o.isBlinded=false and o.id<:lastPage order by o.id desc limit 10")
 	List<OotdDTO> pageMyProfile(@Param("timezone") int timezone, @Param("member") Member member, @Param("lastPage") Long lastPage);
 }
