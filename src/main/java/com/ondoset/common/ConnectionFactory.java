d12aec8f10a078109d2c452f059c944cfaa9e97d
Sujin Kang <1214sujin@naver.com>
2024-06-28 00:54:51 +0900

    fix: DB 관련 설정 정보를 한 곳에서 관리(임시)

--- src/main/java/com/ondoset/common/ConnectionFactory.java
+++ src/main/java/com/ondoset/common/ConnectionFactory.java
@@ -1,21 +1,45 @@
 package com.ondoset.common;
 
 import org.apache.commons.dbcp2.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Component
 public class ConnectionFactory {
 
 	private static BasicDataSource dataSource;
+	private static String url;
+	private static String driver;
+	private static String username;
+	private static String password;
+
+	@Value("${spring.datasource.url}")
+	public void setUrl(String url) {
+		ConnectionFactory.url = url;
+	}
+	@Value("${spring.datasource.driver-class-name}")
+	public void setDriver(String driver) {
+		ConnectionFactory.driver = driver;
+	}
+	@Value("${spring.datasource.username}")
+	public void setUsername(String username) {
+		ConnectionFactory.username = username;
+	}
+	@Value("${spring.datasource.password}")
+	public void setPassword(String password) {
+		ConnectionFactory.password = password;
+	}
 
 	public static Connection getConnection() throws SQLException {
 		if (dataSource == null) {
 			dataSource = new BasicDataSource();
-			dataSource.setUrl("jdbc:mysql://localhost:3306/db24119?serverTimezone=Asia/Seoul");
-			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-			dataSource.setUsername("root");
-			dataSource.setPassword("root");
+			dataSource.setUrl(url);
+			dataSource.setDriverClassName(driver);
+			dataSource.setUsername(username);
+			dataSource.setPassword(password);
 		}
 		return dataSource.getConnection();
 	}
