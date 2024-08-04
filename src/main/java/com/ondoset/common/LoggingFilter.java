573d14b05ba88fb9b73c61e2b746cf0a203830de
Sujin Kang <1214sujin@naver.com>
2024-08-04 23:43:29 +0900

    fix: HTTP 메시지 로깅 시 Jackson 대신 Gson 사용

--- src/main/java/com/ondoset/common/LoggingFilter.java
+++ src/main/java/com/ondoset/common/LoggingFilter.java
@@ -1,8 +1,8 @@
 package com.ondoset.common;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.ondoset.controller.advice.ResponseCode;
 import com.ondoset.controller.advice.ResponseMessage;
 import jakarta.servlet.FilterChain;
@@ -18,7 +18,6 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Iterator;
 
 @Log4j2
 @Component
@@ -50,18 +49,17 @@ public class LoggingFilter extends OncePerRequestFilter {
 
 		// 입력값 로깅
 		StringBuilder requestLog = new StringBuilder();
-		ObjectMapper objectMapper = new ObjectMapper();
 
 		requestLog.append("\n\nrequest = ").append(request.getMethod()).append(" ").append(requestWrapper.getRequestURI()).append("\n");
 		if (!requestWrapper.getParameterMap().isEmpty()) {
 			requestWrapper.getParameterMap().forEach((key, value) -> requestLog.append("\n").append(key).append(" = ").append(Arrays.asList(value).get(0)));
 		}
-		if (requestWrapper.getContentAsByteArray().length != 0) {
-			JsonNode jsonNode = objectMapper.readTree(requestWrapper.getContentAsByteArray());
-			for (Iterator<String> it = jsonNode.fieldNames(); it.hasNext(); ) {
-				String key = it.next();
-				requestLog.append("\n").append(key).append(" = ").append(jsonNode.get(key));
-			}
+		String requestContent = new String(requestWrapper.getContentAsByteArray());
+		if (requestContent.length() != 0) {
+			JsonObject jsonObject = JsonParser.parseString(requestContent).getAsJsonObject();
+			jsonObject.entrySet().forEach(e ->
+					requestLog.append("\n").append(e.getKey()).append(" = ").append(e.getValue())
+			);
 		}
 		log.debug("{}", requestLog.append("\n").toString());
 		ThreadContext.pop();	// 위에서 push한 NDC를 제거
