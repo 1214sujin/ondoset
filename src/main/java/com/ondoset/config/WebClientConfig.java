0db60d7015c104e8299e3b2e41704f985caa0535
Sujin Kang <1214sujin@naver.com>
2025-07-03 22:33:58 +0900

    :bug: fix: WebClient 재시도 및 오류 처리 로직 변경

--- src/main/java/com/ondoset/config/WebClientConfig.java
+++ src/main/java/com/ondoset/config/WebClientConfig.java
@@ -1,12 +1,20 @@
 package com.ondoset.config;
 
+import com.ondoset.controller.advice.CustomException;
+import com.ondoset.controller.advice.ResponseCode;
+import io.netty.channel.ChannelOption;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Log4j2
 @Configuration
@@ -20,7 +28,15 @@ public class WebClientConfig {
 				.filter((request, next) -> {
 					log.debug("기상청에 요청된 URL: {}", request.url().toString());
 					return next.exchange(request)
-							.flatMap(this::handleResponse);
+							.flatMap(this::handleResponse)
+							.retryWhen(Retry.backoff(3, Duration.ofMillis(100))
+									.doBeforeRetry(retrySignal -> log.info("Retrying..."))
+									.onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> new CustomException(ResponseCode.KMA5000))
+							)
+							.doOnError(e -> {
+								log.error("오류가 발생한 요청 API: {}", request.url().toString());
+								log.error(e);
+							});
 				})
 				.build();
 	}
@@ -36,7 +52,15 @@ public class WebClientConfig {
 				.filter((request, next) -> {
 					log.debug("기상청에 요청된 URL: {}", request.url().toString());
 					return next.exchange(request)
-							.flatMap(this::handleResponse);
+							.flatMap(this::handleResponse)
+							.retryWhen(Retry.backoff(3, Duration.ofMillis(100))
+									.doBeforeRetry(retrySignal -> log.info("Retrying..."))
+									.onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> new CustomException(ResponseCode.KMA5000))
+							)
+							.doOnError(e -> {
+								log.error("오류가 발생한 요청 API: {}", request.url().toString());
+								log.error(e);
+							});
 				})
 				.build();
 	}
@@ -45,12 +69,11 @@ public class WebClientConfig {
 		if (response.statusCode().isError() || response.headers().header("Content-Type").get(0).startsWith("text/xml")) {
 			return response.bodyToMono(String.class)
 					.flatMap(body -> {
-						log.debug("\n{}", body);
+						log.error("오류 응답 발생:\n{}", body);
 						return Mono.error(new RuntimeException());
 					});
 		} else {
 			return Mono.just(response);
 		}
 	}
-
 }
